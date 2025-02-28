4f3a776d7876f96c58073fa414585cb18f41c5de
hak1319 <gkr054@naver.com>
2025-02-28 10:59:34 +0900

    Test with Coverage

--- oz_django8/urls.py
+++ oz_django8/urls.py
@@ -2,13 +2,8 @@ from django.contrib import admin
 from django.http import HttpRequest
 from django.urls import URLPattern, path
 
-
 from tabom.services.like_service import do_like
-
-
-
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-
 ]
